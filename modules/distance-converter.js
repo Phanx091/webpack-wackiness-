c82002805beba40d2e8922c05db98f7c55912075
Jason Phan <Phanx091@umn.edu>
2018-05-14 16:44:03 -0500

    completed

--- modules/distance-converter.js
+++ modules/distance-converter.js
@@ -1,9 +1,13 @@
 const milesToKilometers = miles => {
     return miles * 1.60934;
 };
+const conversion = inches => {
+    return inches * 0.393701;
 
+}
 const distanceConverter = {
     milesToKilometers,
+    conversion,
 };
 
 export default distanceConverter;
---
+++ modules/time-formatter.js
@@ -0,0 +1,8 @@
+import moment from 'moment';
+
+const dateConverter = moment().format('dddd, MMMM Do YYYY');
+
+
+
+
+export default dateConverter; 
